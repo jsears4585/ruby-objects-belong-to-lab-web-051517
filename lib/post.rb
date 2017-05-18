31aa8c6c8949177d86756f8bb48c8c4cff2f4eb1
Jason Sears <jsears4585@gmail.com>
2017-05-18 10:25:01 -0400

    Finished.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+    self.title = title
+    self.author = author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    self.title = title
+    self.artist = artist
+  end
+end
